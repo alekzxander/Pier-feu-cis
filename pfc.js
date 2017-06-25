53ee56ef7ec814b23a88a12d82bb95690c20296a
Alexandre Callens <alekzxand@gmail.com>
2017-06-25 22:09:54 +0400

    modification max.random

--- pfc.js
+++ pfc.js
@@ -1,35 +1,53 @@
-function jouer(){
-var useur = prompt("Entrez votre nom")
-var choixUser = prompt("Choisissez pierre; feuille ou ciseaux")}
+
+
 var scoreUser = 0;
 var scoreOrdi = 0;
+function jouer(){
+var user = prompt("Entrez votre nom")
+var choixUser = prompt("Choisissez pierre, feuille ou ciseaux")
 
+}
+	/*	Ca marche pas !!			
 					
-					
-var choixOrdi = Math.floor(Math.random () *60)
-		if (choixOrdi <= 20){
+var choixOrdi = Math.floor(Math.random() *60);
+		if (choixOrdi < 20){
 			choixOrdi = "pierre";
-		}else if (choixOrdi <= 40) && (choixOrdi > 20){
+		}else if (choixOrdi < 40) && (choixOrdi > 20){
 			choixOrdi = "ciseaux";
 		}else {
 			choixOrdi = "feuille";
 		}	
-						
+*/
+//utilisation d'une autre technique pour avoir un choix aléatoire
+var ordi = ["pierre", "feuille", "ciseaux"];
+var choix = ordi.length;
+choixOrdi = ordi[Math.random() * choix | 0];
+alert(choixOrdi);
+
+
+	
+
+
 function comparer(choixUser, choixOrdi){
 		 if (choixUser === choixOrdi){
-		 	alert('Egalité')
+		 	alert("Egalité")
+		 	
 		 }
-		 else if (choixUser === "pierre") && (choixOrdi === "ciseaux") || (choixUser === "ciseaux") && (choixOrdi === "feuille" ) || (choixUser === "feuille") && (choixOrdi === "pierre"){
+
+		 else if (choixUser === "pierre" && choixOrdi === "ciseaux") || (choixUser === "ciseaux" && choixOrdi === "feuille" ) || (choixUser === "feuille" && choixOrdi === "pierre"){
 		 	alert("Vous avez gagné ! l'ordinateur a choisi" + choixOrdi );
 		 	scoreUser + 5;
 
 		 }
-		 else if (choixOrdi === "ciseaux") && (choixUser === "feuille") || (choixOrdi === "pierre") && (choixUser === "ciseaux") || (choixOrdi === "feuille") && (choixUser === "pierre"){
+		 else if (choixOrdi === "ciseaux" && choixUser === "feuille") || (choixOrdi === "pierre" && choixUser === "ciseaux") || (choixOrdi === "feuille" && choixUser === "pierre"){
 		 	alert("Vous avez perdu ! L'ordinateur a choisi" + choixOrdi);
 		 	scoreOrdi + 5;
 		 }
+		 else {
+		 	alert("putin")
+		 }
+		}
 
 
-alert("Votre score est de " + scoreUser + "contre " + scoreOrdi +" pour l'ordinateur !")
 
-}
+alert("Le score de" + user + " est de " + scoreUser + "contre " + scoreOrdi + " pour l'ordinateur !")
